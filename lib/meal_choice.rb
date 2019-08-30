43570ffd0d4f15c65464625eaa30ff4184d8584a
Joshua Rodriguez <josh030596@gmail.com>
2019-08-30 13:06:13 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,7 +5,7 @@ end
 def meal_choice(protein = 'meat', veg1, veg2)
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   
-  expect(meal_choice("peas", "carrots"))).to eq("A plate of meat with peas and carrots.")
+  expect(meal_choice("peas", "carrots")).to eq("A plate of meat with peas and carrots.")
 end
 
 meal_choice("peas", "carrots")
